8f00e80adaad6e55128ed3f63acc0c6f38df039e
Olga <olgaburik55@gmail.com>
2023-10-25 18:10:31 +0300

    work form done

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,207 @@
-
-document.addEventListener('DOMContentLoaded', function(){
-    let formData = {};
-    const form =document.querySelector('form');
-    const LS = localStorage;
-    var nameFull = document.getElementById("resultFormName");
+// let formData = {};
+// const form =document.querySelector('form');
+// const LS = localStorage;
+// console.log(form);
+// // заношу данные из формы в localstorage
+// form.addEventListener('input', function(event){
+//     formData[event.target.name] = event.target.value;
+//     LS.setItem('formData', JSON.stringify(formData));
+// })
+// // востанавливаю данные после обновления страницы
+// if (LS.getItem('formData')) {
+//     formData = JSON.parse(LS.getItem('formData'));
+//     for (let key in formData) {
+//         if (form.elements[key]) {
+//             if (form.elements[key]?.type === 'checkbox' && formData[key] ==='on') {
+//                 form.elements[key].checked = true;
+//             }
+//             else {
+//                 form.elements[key].value = formData[key];
+//                 resultFormName.value = fillFormName.value;
+//                 resultFormDescr.value = fillFormDescr.value;
+//                 if(resultFormName.value != ''){
+//                     document.querySelector("#resultFormName").style.background = "none";
+//                 }
+//                 if(resultFormDescr.value != ''){
+//                     document.querySelector("#resultFormDescr").style.background = "none";
+//                 }
+//                 console.log(resultFormName.value);
+//                 console.log(fillFormName.value);
+//             }
+//         }
+//     }
     
+// }
 
-    // востанавливаю данные после обновления страницы
-    form.addEventListener('input', function(event){
-        formData[event.target.name] = event.target.value;
-        LS.setItem('formData', JSON.stringify(formData));
-    })
 
-    if (LS.getItem('formData')) {
-        formData = JSON.parse(LS.getItem('formData'));
-        for (let key in formData) {
-            if (form.elements[key]) {
-                if (form.elements[key]?.type === 'checkbox' && formData[key] ==='on') {
-                    form.elements[key].checked = true;
+// // обновляю данные в окне предпросмотра с каждым нажатием клавиши
+// $("#fillFormName").keyup(function() {
+//     resultFormName.value = this.value;
+//     // console.log(nameFull.value);
+//     if(resultFormName.value != ''){
+//         document.querySelector("#resultFormName").style.background = "none";
+//     }
+//     if(resultFormName.value == ''){
+//         document.querySelector("#resultFormName").style.background = "var(--eeeef-1-filter-hover, #EEEEF1)";
+//     }
+// });
+// $("#fillFormDescr").keyup(function() {
+//     resultFormDescr.value = this.value;
+//     // console.log(nameFull.value);
+//     if(resultFormDescr.value != ''){
+//         document.querySelector("#resultFormDescr").style.background = "none";
+//     }
+//     if(resultFormDescr.value == ''){
+//         document.querySelector("#resultFormDescr").style.background = "var(--eeeef-1-filter-hover, #EEEEF1)";
+//     }
+// });
+
+// if (form){
+//     for (let key in formData) {
+//         if (form.elements[key]) {
+//             form.elements[key].value = formData[key];
+//             if(form.querySelector("#fillFormWork")) {
+//                 document.querySelector("#resultFormWork").value = form.querySelector("#resultFormWork").value;
+//                 console.log(form.querySelector("#fillFormWork").value);
+//                 console.log(document.querySelector("#resultFormWork").value);
+//             }
+            
+//             console.log(formData);
+//             console.log( form.elements.value);
+//             console.log(formData);
+            
+//         } 
+        
+//     }
+// }
+let formData = {};
+const form =document.querySelectorAll('form');
+const LS = localStorage;
+console.log(form);
+// заношу данные из формы в localstorage
+form[0].addEventListener('input', function(event){
+    formData[event.target.name] = event.target.value;
+    LS.setItem('formData', JSON.stringify(formData));
+})
+// востанавливаю данные после обновления страницы
+if (LS.getItem('formData')) {
+    formData = JSON.parse(LS.getItem('formData'));
+    for (let key in formData) {
+        if (form[0].elements[key]) {
+            if (form[0].elements[key]?.type === 'checkbox' && formData[key] ==='on') {
+                form[0].elements[key].checked = true;
+            }
+            else {
+                form[0].elements[key].value = formData[key];
+                resultFormName.value = fillFormName.value;
+                resultFormDescr.value = fillFormDescr.value;
+                if(resultFormName.value != ''){
+                    document.querySelector("#resultFormName").style.background = "none";
                 }
-                else {
-                    form.elements[key].value = formData[key];
-                    resultFormName.value = fillFormName.value;
-                    nameFull.value = fillFormName.value;
-                    if(nameFull.value != ''){
-                        document.querySelector("#resultFormName").style.background = "none";
-                    }
+                if(resultFormDescr.value != ''){
+                    document.querySelector("#resultFormDescr").style.background = "none";
                 }
+                console.log(resultFormName.value);
+                console.log(fillFormName.value);
             }
         }
+    }
+    
+}
+
+
+// обновляю данные в окне предпросмотра с каждым нажатием клавиши
+$("#fillFormName").keyup(function() {
+    resultFormName.value = this.value;
+    if(resultFormName.value != ''){
+        document.querySelector("#resultFormName").style.background = "none";
+    }
+    if(resultFormName.value == ''){
+        document.querySelector("#resultFormName").style.background = "var(--eeeef-1-filter-hover, #EEEEF1)";
+    }
+});
+$("#fillFormDescr").keyup(function() {
+    resultFormDescr.value = this.value;
+    if(resultFormDescr.value != ''){
+        document.querySelector("#resultFormDescr").style.background = "none";
+    }
+    if(resultFormDescr.value == ''){
+        document.querySelector("#resultFormDescr").style.background = "var(--eeeef-1-filter-hover, #EEEEF1)";
+    }
+});
+// обновляю данные в окне предпросмотра которые будут введены на следующих страницах
+if (form[1]){
+    for (let key in formData) {
+        if (form[1].elements[key]) {
+            form[1].elements[key].value = formData[key];
+            if(form[1].querySelector("#fillFormWork")) {
+                document.querySelector("#resultFormWork").value = form[1].querySelector("#resultFormWork").value;
+                console.log(formData);
+                console.log(formData);
+            }
+        } 
         
     }
+}
+const widthWork = document.querySelector("#resultFormWork");
+widthWork.style.width = 0;
+widthWork.value = document.querySelector("#resultFormWork").value;
+widthWork.style.width = widthWork.scrollWidth + "px";
 
-    // обновляю данные в окне предпросмотра с каждым нажатием клавиши
-    $("#fillFormName").keyup(function() {
-        nameFull.value = this.value;
-        // console.log(nameFull.value);
-        if(nameFull.value != ''){
-            document.querySelector("#resultFormName").style.background = "none";
-        }
-        if(nameFull.value == ''){
-            document.querySelector("#resultFormName").style.background = "var(--eeeef-1-filter-hover, #EEEEF1)";
-        }
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// развернуть о себе
+// document.querySelector("#more").addEventListener('click', function(){
+//     document.querySelector(".bio_text").classList.toggle('active');
     
-})
-
-
+// });
+document.querySelector("#more").addEventListener('click', function(){
+    const bioText = document.querySelector(".bio_text");
+    bioText.classList.toggle('active');
+    const moreAfter = document.querySelector("#more::after");
+    if (bioText.classList.contains('active')) {
+        moreAfter.style.content = "url('../../icons/rectanBack.svg')";
+    } else {
+        moreAfter.style.content = "url('../../icons/rectan.svg')";
+    }
+    alert('ok');
+});
 
 
 
---
+++ src/js/script2.js
@@ -0,0 +1,54 @@
+const LS = localStorage;
+const form =document.querySelectorAll('form');
+formData = JSON.parse(LS.getItem('formData'));
+// заношу данные в сетлокейл
+form[0].addEventListener('input', function(event){
+    formData[event.target.name] = event.target.value;
+    LS.setItem('formData', JSON.stringify(formData));
+})
+
+// вставляю в предпросмотр значения, которые были введены в предыдущей вкладке
+if (form[1]){
+    for (let key in formData) {
+        if (form[1].elements[key]) {
+            form[1].elements[key].value = formData[key];
+            if(form[1].querySelector("#fillFormName")) {
+                document.querySelector("#resultFormName").value = form[1].querySelector("#fillFormName").value;
+            }
+            if(form[1].querySelector("#fillFormDescr")) {
+                document.querySelector("#resultFormDescr").value = form[1].querySelector("#fillFormDescr").value;
+            }
+        }
+        
+    }
+}
+// вставляю в предпросмотр значения, которые были введены на этой вкладке
+if (form[0]){
+    for (let key in formData) {
+        if (form[0].elements[key]) {
+            if (form[0].elements[key]?.type === 'checkbox' && formData[key] ==='on') {
+                form[0].elements[key].checked = true;
+            }
+            else {
+                form[0].elements[key].value = formData[key];
+                resultFormWork.value = fillFormWork.value;
+            }
+        }
+    }
+}
+// обнобляю с каждым нажатие клавиши поле работа
+$("#fillFormWork").keyup(function() {
+    resultFormWork.value = this.value;
+    if(resultFormWork.value == ''){
+        document.querySelector("#resultFormWork").style.background = "var(--eeeef-1-filter-hover, #EEEEF1)";
+    }
+    const widthWork = document.querySelector("#resultFormWork");
+    widthWork.style.width = 0;
+    widthWork.value = document.querySelector("#resultFormWork").value;
+    widthWork.style.width = widthWork.scrollWidth + "px";
+});
+const widthWork = document.querySelector("#resultFormWork");
+widthWork.style.width = 0;
+widthWork.value = document.querySelector("#resultFormWork").value;
+widthWork.style.width = widthWork.scrollWidth + "px";
+
