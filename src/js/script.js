b95a00f9e22822f95331a1b45411badaf1e46413
Olga <olgaburik55@gmail.com>
2023-11-03 17:42:33 +0300

    bad calendar

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+
 let formData = {};
 let url;
 const form =document.querySelectorAll('form');
@@ -11,7 +11,6 @@ const photoContainer2 = document.querySelector('.form_fill > .photo');
 
 // загружаю фото в окно предпросмотра
 image_input.addEventListener('change', function (e) {
-    console.log(image_input.files);
     const reader = new FileReader();
     reader.onload = function () {
         const img = new Image();
@@ -19,18 +18,14 @@ image_input.addEventListener('change', function (e) {
         url = reader.result;
         photoContainer.appendChild(img);
         localStorage.setItem('savedImage', url);
-        console.log(url);
         location.reload();
     } 
     reader.readAsDataURL(image_input.files[0]);
 }, false)
-console.log(url);
 // загружаю фото на место кнопки загрузить 
 // добавляю крестик для сброса фото
 const savedImage = localStorage.getItem('savedImage');
-console.log(savedImage);
 if (savedImage!="") {
-    console.log(savedImage);
     const deletePhoto = document.querySelector('.delete_photo');
 
     deletePhoto.addEventListener('click', function() {
@@ -41,7 +36,6 @@ if (savedImage!="") {
         }
         location.reload();
     });
-    console.log(savedImage);
     const img = new Image();
     img.src = savedImage;
     photoContainer.appendChild(img);
@@ -79,13 +73,13 @@ form[0].addEventListener('input', function(event){
 if (LS.getItem('formData')) {
     formData = JSON.parse(LS.getItem('formData'));
     for (let key in formData) {
-        console.log(formData);
-        if (form[0].elements[key]) {
-            console.log(form[1].elements[key]);
+        if (form[0].elements[key] && key!="Age") {
             if (form[0].elements[key]?.type === 'checkbox' && formData[key] ==='on') {
                 form[0].elements[key].checked = true;
             }
             else {
+                console.log(key);
+                console.log(formData[key]);
                 form[0].elements[key].value = formData[key];
                 resultFormName.value = fillFormName.value;
                 resultFormDescr.value = fillFormDescr.value;
@@ -113,8 +107,6 @@ if (LS.getItem('formData')) {
                 if(resultFormDescr.value != ''){
                     document.querySelector("#resultFormDescr").style.background = "none";
                 }
-                console.log(resultFormName.value);
-                console.log(fillFormName.value);
             }
         }
     }
@@ -148,8 +140,6 @@ if (form[1]){
             form[1].elements[key].value = formData[key];
             if(form[1].querySelector("#fillFormWork")) {
                 document.querySelector("#resultFormWork").value = form[1].querySelector("#resultFormWork").value;
-                console.log(formData);
-                console.log(formData);
             }
         } 
         
@@ -163,47 +153,73 @@ const widthGender = document.querySelector("#resultFormGender");
 widthGender.style.width = 0;
 widthGender.style.width = widthGender.scrollWidth + "px";
 
+// календарь
+var year;
+var month;
+var date;
+if (document.querySelector("#resultFormAge").value == "") {
+    document.querySelector("#resultFormAge").value = `NaN лет`;
+    let widthAge = document.querySelector(".age_result");
+    widthAge.style.width = 0;
+    widthAge.style.width = widthAge.scrollWidth + "px";
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const datePicker = flatpickr("input[type=datetime-local]", {
+        dateFormat: "d.m.Y",
+        static: false,
+        onChange: function(selectedDates, dateStr, instance) {
+            selectedDates.forEach(function (date){
+                console.log(date.getFullYear(), date.getMonth()+1, date.getDate());
+                year = date.getFullYear();
+                month = date.getMonth();
+                date = date.getDate();
+                var now = new Date(); //Текущя дата
+                var today = new Date(now.getFullYear(), now.getMonth(), now.getDate()); //Текущя дата без времени
+                var dob = new Date(year, month, date); //Дата рождения
+                var dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate()); //ДР в текущем году
+                var age; //Возраст
+
+                //Возраст = текущий год - год рождения
+                age = today.getFullYear() - dob.getFullYear();
+                //Если ДР в этом году ещё предстоит, то вычитаем из age один год
+                console.log(today);
+                console.log(dobnow);
+                if (today < dobnow) {
+                    age = age-1;
+                }
+                localStorage.setItem('savedAge', age);
+                localStorage.setItem('savedDate', `${date}.${month+1}.${year}`);
+                console.log(`Возраст: ${age}`);
+                const savedAge = localStorage.getItem('savedAge');
+                console.log(savedAge);
+                document.querySelector("#resultFormAge").value = `${savedAge} лет`;
+                let widthAge = document.querySelector(".age_result");
+                widthAge.style.width = 0;
+                widthAge.style.width = widthAge.scrollWidth + "px";
+            });
+
+            // Извлекаем сохраненные данные из localStorage
+            const savedAge = localStorage.getItem('savedAge');
+            const savedDate = localStorage.getItem('savedDate');
+
+            // Устанавливаем сохраненные значения в поля формы
+            if (savedAge) {
+                document.querySelector("#resultFormAge").value = `${savedAge} лет`;
+            }
+            if (savedDate) {
+                const dateInputElement = document.querySelector("input[type=datetime-local]");
+                if (dateInputElement) {
+                    dateInputElement.value = savedDate;
+                }
+            }
+        }
+    });
 });
 
 
 
-
-
-
-
-
-
-// const image_input = document.querySelector("#image_input");
-// var uploaded_image = "";
-
-
-// image_input.addEventListener("change", function(){
-
-//     // console.log(image_input.value);
-
-//     const reader = new FileReader();
-//     reader.addEventListener("load", () => {
-//         uploaded_image = reader.result;
-//         document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`; 
-        
-//     });
-//     reader.readAsDataURL(this.files[0]);
-// })
-
-
-
-// window.addEventListener('load', function() {
-//     document.querySelector('input[type="file"]').addEventListener('change', function() {
-//         if (this.files && this.files[0]) {
-//             var img = document.querySelector('img');
-//             img.onload = () => {
-//                 URL.revokeObjectURL(img.src);  // no longer needed, free memory
-//             }
-  
-//             img.src = URL.createObjectURL(this.files[0]); // set src to blob url
-//         }
-//     });
-//   });
 
 
 
